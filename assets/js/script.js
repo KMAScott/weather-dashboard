6de3408ebb126e5cd399991a0efb7d4dab0ffc42
Scott Ashley <burnbright987@gmail.com>
2022-05-05 12:20:41 -0400

    added current conditions data

---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+var APIkey = "feac6823ef10156c3b50861b18206856";
+
+var cityInput = $("#cityInput")
+var searchButton = $("#searchButton")
+var clearButton = $("#clearHistoryBtn")
+var pastSearchCities = $("#pastSearches")
+
+function getWeather(data) {
+    var openWeatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&appid=${APIkey}"
+    fetch(openWeatherUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            var currentConditionsEl = $("#currentConditions");
+            currentConditionsEl.addClass("border border-primary");
+
+            var cityNameEl = $("<h2>");
+            cityNameEl.text(currentCity);
+            currentConditionsEl.append(cityNameEl);
+
+            var currentCityDate = data.current.dt;
+            currentCityDate = dayjs().get('date').format(MM/DD/YYYY);
+            var currentDateEl = $("<span>");
+            currentDateEl.text("(currentCityDate)");
+            cityNameEl.append(currentDateEl);
+
+            var currentCityWeatherIcon = data.current.weather[0].icon;
+            var currentWeatherIconEl = $("<img>");
+            currentWeatherIconEl.attr("src", "http://openweathermap.org/img/wn/" + currentCityWeatherIcon + ".png");
+            cityNameEl.append(currentWeatherIconEl);
+
+            var currentCityTemp = data.current.temp;
+            var currentTempEl = $("<p>");
+            currentTempEl.text("Temp: ${currentCityTemp}°F");
+            currentConditionsEl.append(currentTempEl);
+
+            var currentCityWind = data.current.wind_speed;
+            var currentWindEl = $("<p>");
+            currentWindEl.text("Wind: ${currentCityWind} MPH");
+            currentConditionsEl.append(currentWindEl);
+
+            var currentCityHumidity = data.current.humidity;
+            var currentHumidityEl = $("<p>");
+            currentHumidityEl.text("Humidity: ${currentCityHumidity}%");
+            currentConditionsEl.append(currentHumidityEl);
+
+            var currentCityUv = data.current.uvi;
+            var currentUvEl = $("<p>");
+            var currentUvSpanEl = $("<span>");
+            currentUvEl.append(currentUvSpanEl);
+            currentUvSpanEl.text("UV: ${currentCityUv}");
+
+            if (currentCityUv < 3) {
+                currentUvSpanEl.css({"background-color":"green", "color":"white"});
+            } else if (currentCityUv < 6) {
+                currentUvSpanEl.css({"background-color":"yellow", "color":"black"});
+            } else if (currentCityUv < 8) {
+                currentUvSpanEl.css({'background-color':'orange', 'color':'white'});
+            } else if (currentCityUv < 11) {
+                currentUvSpanEl.css({'background-color':'red', 'color':'white'});
+            } else {
+                currentUvSpanEl.css({'background-color':'violet', 'color':'white'});
+            }
+
+            currentConditionsEl.append(currentUvEl);
+        })
+}
